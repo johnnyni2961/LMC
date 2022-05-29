1e9dd3cf34a4c721b9a718b9bf03697287907a91
JohnnyNi <68974700+johnnyni2961@users.noreply.github.com>
2022-05-28 23:30:05 -0700

    Delete src directory

--- src/components/caption/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Caption(props) {
-  return (
-    <div className="caption-wrapper mb-[28px]">
-      <h2 className="caption lightest_gold">{props.caption}</h2>
-    </div>
-  )
-}
